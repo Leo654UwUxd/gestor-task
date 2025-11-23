06c0918a2b88c435fd6fbbcbe87467d5f8525fe5
Leo654UwUxd <leonardomunoz479@gmail.com>
2025-11-23 17:47:35 -0400

    Create App.vue with basic structure and TaskList

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1>Gestor de Tareas (TypeScript)</h1>
+  <TaskList />
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import TaskList from "./components/TaskList.vue";
+
+export default defineComponent({
+  components: { TaskList }
+});
+</script>
+
+<style>
+body {
+  font-family: Arial, sans-serif;
+  padding: 20px;
+}
+</style>
